0585410cf1ddfb6873772d20acef802fb9742cba
igor7301 <codtQl1fkybgh>
2015-03-11 00:15:10 +0600

    allure listener fix

---
+++ src/main/java/com/application/AllureFailureTestListener.java
@@ -0,0 +1,47 @@
+package com.application;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+/**
+ * Created by Admin on 10.03.15.
+ */
+public class AllureFailureTestListener implements ITestListener {
+    @Override
+    public void onTestStart(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onTestFailure(ITestResult iTestResult) {
+        System.out.println("Allure listener for failed tests");
+        Utils.makeScreenshot("Test_screenshot");
+
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onStart(ITestContext iTestContext) {
+
+    }
+
+    @Override
+    public void onFinish(ITestContext iTestContext) {
+
+    }
+}
